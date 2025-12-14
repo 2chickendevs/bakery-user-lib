55db22a4770e91a47379c016f6f38cce7a5fe2ea
ianvortech <ian.nguyen@vortechinc.io>
2025-12-14 10:40:58 +0700

    Init project
    Init project

---
+++ src/main/java/com/twochickendevs/bakeryuserlib/BakeryUserLibApplication.java
@@ -0,0 +1,13 @@
+package com.twochickendevs.bakeryuserlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryUserLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryUserLibApplication.class, args);
+	}
+
+}
