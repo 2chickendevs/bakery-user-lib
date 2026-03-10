769a96655dfaf9f3f41741b33f0b30a23aa9ef81
nntan041299 <nntan041299@gmail.com>
2026-03-10 16:33:06 +0700

    Init source code

---
+++ src/main/java/com/twochickendevs/bakeryuserlib/service/CurrentPrincipal.java
@@ -0,0 +1,20 @@
+package com.twochickendevs.bakeryuserlib.service;
+
+import com.twochickendevs.bakeryuserlib.model.UserAuthenticationToken;
+import com.twochickendevs.bakeryuserlib.model.UserInfo;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrentPrincipal {
+
+    public UserInfo getUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            UserAuthenticationToken token = (UserAuthenticationToken) authentication;
+            return token.getUserInfo();
+        }
+        return null;
+    }
+}
